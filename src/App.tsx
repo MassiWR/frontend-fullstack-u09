b3e5bd4e161350a6c84397642924c1eb8b4b61d5
MassiWR <90767729+MassiWR@users.noreply.github.com>
2022-10-30 21:21:40 +0100

    add loggedIn and out components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './App.css';
 import { RegisterPage } from './Pages/RegisterPage';
+import { checkIsLoggedIn } from './services/auth.service';
 
 function App() {
+
+  const [isLoggedIn, setisLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const isLoggedIn = checkIsLoggedIn();
+    setisLoggedIn(isLoggedIn);
+  
+  }, [])
+  
+
+
+
   return (
     <div className="App">
-      <div>Working</div>
-      <RegisterPage></RegisterPage>
+      <nav>
+        <Link to={'/'} >Home</Link>
+        <Link to={'/about'}>About</Link>
+
+      {isLoggedIn && <loggedInMenu />}
+      {isLoggedIn || <LoggedOutMenu />}
+
+      </nav>
+      
     </div>
   );
 }
---
+++ src/Components/LoggedOutMenu.tsx
@@ -0,0 +1,14 @@
+import react, { FC } from "react"
+import { Link } from "react-router-dom"
+
+
+
+
+
+export const LoggedOutMenu: FC = () => {
+
+    return (<>
+        <Link to={"/login"}>Login</Link>
+        <Link to={"/register"}>Register</Link>
+    </>)
+}
