d6280f16c5e453f637b194762ab4a427638a7566
MassiWR <90767729+MassiWR@users.noreply.github.com>
2022-11-05 21:31:29 +0100

    add material ui appbar

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -2,8 +2,8 @@ import react, { FC, useEffect, useState } from "react"
 import AuthService from "../../services/auth.service";
 import { LoggedInMenu } from "./LoginMenu/LoggedInMenu";
 import { LoggedOutMenu } from "./LoginMenu/LoggedOutMenu";
-
-
+import { AppBar, Toolbar, IconButton, Typography } from "@mui/material";
+import CalendarMonthTwoToneIcon from '@mui/icons-material/CalendarMonthTwoTone';
 
 export const Navbar: FC = () => {
     const [isLoggedIn, setisLoggedIn] = useState(false);
@@ -17,11 +17,15 @@ export const Navbar: FC = () => {
 
     return (<>
         
-        <nav className="navigation">
-          <div className="topnav">
-        {isLoggedIn  ?  <LoggedInMenu /> : <LoggedOutMenu />}
-          </div>
-        </nav>
+       <AppBar position="static" >
+        <Toolbar>
+          <IconButton size="large" edge="start" color="inherit" aria-label="logo">
+            <CalendarMonthTwoToneIcon />
+          </IconButton>
+          <Typography variant="h6" component='div'>
+            BookR</Typography>
+        </Toolbar>
+       </AppBar>
         
     </>)
 }
